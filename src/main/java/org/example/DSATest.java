7f1f776817e6d76f1f1b279652bce0f9c19c2ebf
张坤 <kunzkun@163.com>
2022-08-22 17:35:45 +0800

    从字符串中读取私钥和公钥

--- src/main/java/org/example/DSATest.java
+++ src/main/java/org/example/DSATest.java
@@ -1,13 +1,10 @@
 package org.example;
 
 import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
-import javax.crypto.Cipher;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.StringReader;
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
@@ -23,7 +20,9 @@ import java.security.spec.X509EncodedKeySpec;
  * openssl pkcs8 -topk8 -in dsa_private_key.pem -out pkcs8_dsa_private_key.pem -nocrypt
  * */
 public class DSATest {
-    private static final String PRIVATE_KEY = "-----BEGIN PRIVATE KEY-----\n" +
+    private static final String PRIVATE_KEY_PEM = "/Users/j/Desktop/dsa/pkcs8_dsa_private_key.pem";
+    private static final String PUBLIC_KEY_PEM = "/Users/j/Desktop/dsa/dsa_public_key.pem";
+    private static final String PRIVATE_KEY_PKCS8 = "-----BEGIN PRIVATE KEY-----\n" +
             "MIIBSgIBADCCASsGByqGSM44BAEwggEeAoGBAMNUsRMfJ32hCOnjO3UpEeEjebnp\n" +
             "GBsQmawr5PzZgmNrfgKJlzHD44qL9qlMhua8xH7dRCqpcxbnuCd+g5jLVQ1KOwR6\n" +
             "0h7vlLt3YY7wZJyArFgiEi7+j8rkQWhaSJD3TbqMRb7EVJgRVtLv5GNM5YhA30hB\n" +
@@ -32,20 +31,32 @@ public class DSATest {
             "CzVXk9hFPIpqrUscHtb3llecYRvuyifyjtYiGyhYBLn1qzrqIQpyarEGg8KzT6Lt\n" +
             "3d8shwfyrFrPp6613HL31iqo+v35gQQWAhQiBsB/8T/VYj+3czgdvmmjOWsZ1g==\n" +
             "-----END PRIVATE KEY-----\n";//测试生成的DSA私钥
-    private static final String PRIVATE_KEY_PEM = "/Users/j/Desktop/dsa/pkcs8_dsa_private_key.pem";
-    private static final String PUBLIC_KEY_PEM = "/Users/j/Desktop/dsa/dsa_public_key.pem";
+    private static final String PUBLIC_KEY_STRING
+            = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIIBtzCCASsGByqGSM44BAEwggEeAoGBAMNUsRMfJ32hCOnjO3UpEeEjebnpGBsQ\n" +
+            "mawr5PzZgmNrfgKJlzHD44qL9qlMhua8xH7dRCqpcxbnuCd+g5jLVQ1KOwR60h7v\n" +
+            "lLt3YY7wZJyArFgiEi7+j8rkQWhaSJD3TbqMRb7EVJgRVtLv5GNM5YhA30hBiwOx\n" +
+            "OANrEZKMotjzAhUAi12iG05Clv5yhb1jLnS/GJaBR5MCgYALDQ14AKCUpxYLCoua\n" +
+            "huvpNSNlHOTN8mZi4NUw590XZRJSUHMra3xMbeq4NcUEhq0+vZf8LpWI6rgkCzVX\n" +
+            "k9hFPIpqrUscHtb3llecYRvuyifyjtYiGyhYBLn1qzrqIQpyarEGg8KzT6Lt3d8s\n" +
+            "hwfyrFrPp6613HL31iqo+v35gQOBhQACgYEAoEh8mAJgO5rOuk9nthZwLILTZF6J\n" +
+            "DhGFVRdgmU3Q2lD2b6b0g667D2z4L7Ojk7eWpfB1AapD+jTCbZrasl2UfKn4F9Ua\n" +
+            "xQXb4zui2lpIYURjpLLf48I6Kjuq0VVKZzDi+iIs1m1FQO2eInfRMUNPoOlNJXsi\n" +
+            "a6GlheOukVAfdks=\n" +
+            "-----END PUBLIC KEY-----\n";
 
     public static void main(String[] args) throws Exception {
-        PrivateKey privateKey = getPrivateKeyFromPem();
-//        PrivateKey privateKey = getPrivateKeyFromString();
-        PublicKey publicKey = getPublicKeyFromPem();
+//        PrivateKey privateKey = getPrivateKeyFromPem();
+//        PublicKey publicKey = getPublicKeyFromPem();
+        PrivateKey privateKey = getPrivateKeyFromString();
+        PublicKey publicKey = getPublickKeyFromString();
         String message = "hello world";
         byte[] signMessage = signMessage(privateKey, message.getBytes());
         boolean result = verifySignature(publicKey, message.getBytes(), signMessage);
         System.out.println(result);
     }
 
-    // 获取私匙（通过pem文件）
+    // 获取私匙 from pem file
     private static PrivateKey getPrivateKeyFromPem() throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(PRIVATE_KEY_PEM));
         String s = br.readLine();
@@ -64,9 +75,9 @@ public class DSATest {
         PrivateKey privateKey = kf.generatePrivate(keySpec);
         return privateKey;
     }
-    // 获取私匙（通过字符串）
+    // 获取私匙 from string
     private static PrivateKey getPrivateKeyFromString() throws Exception {
-        BufferedReader br = new BufferedReader(new StringReader(PRIVATE_KEY));
+        BufferedReader br = new BufferedReader(new StringReader(PRIVATE_KEY_PKCS8));
         String s = br.readLine();
         String str = "";
         s = br.readLine();
@@ -84,9 +95,26 @@ public class DSATest {
         return privateKey;
     }
 
-    // 获取公钥
+    // 获取公钥 from pem file
     private static PublicKey getPublicKeyFromPem() throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(PUBLIC_KEY_PEM));
+        String s = br.readLine();
+        String str = "";
+        s = br.readLine();
+        while (s.charAt(0) != '-') {
+            str += s + "\r";
+            s = br.readLine();
+        }
+        BASE64Decoder base64decoder = new BASE64Decoder();
+        byte[] b = base64decoder.decodeBuffer(str);
+        KeyFactory kf = KeyFactory.getInstance("DSA");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(b);
+        PublicKey pubKey = kf.generatePublic(keySpec);
+        return pubKey;
+    }
+    // 获取公钥 from string
+    private static PublicKey getPublickKeyFromString() throws Exception {
+        BufferedReader br = new BufferedReader(new StringReader(PUBLIC_KEY_STRING));
         String s = br.readLine();
         String str = "";
         s = br.readLine();
--- src/main/java/org/example/RSATest.java
+++ src/main/java/org/example/RSATest.java
@@ -6,10 +6,12 @@ import sun.misc.BASE64Encoder;
 import javax.crypto.Cipher;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+
 /**
  * 以下命令生成的文件会存放在当前目录下
  * -- 生成私钥命令
@@ -18,18 +20,45 @@ import java.security.spec.X509EncodedKeySpec;
  * openssl rsa -in rsa_private_key.pem -pubout -out rsa_public_key.pem
  * -- 对私钥进行pkcs8编码
  * openssl pkcs8 -topk8 -in rsa_private_key.pem -out pkcs8_rsa_private_key.pem -nocrypt
- * */
+ */
 public class RSATest {
     public static final String PRIVATE_KEY_PEM = "/Users/j/Desktop/rsa/pkcs8_rsa_private_key.pem";
     public static final String PUBLIC_KEY_PEM = "/Users/j/Desktop/rsa/ras_public_key.pem";
+    public static final String PRIVATE_KEY_PCKS8
+            = "-----BEGIN PRIVATE KEY-----\n" +
+            "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAMZbTbPEN3jtzla3\n" +
+            "Ls/a2HDYN/bif0BujgHmllWDjwEoAqFDZrgpI17Y3xY6jMFs5amY77vg3K19obCl\n" +
+            "ScVM1Y4eYsMhyxHzuF3uju2Xd891BIeHSdHDtWQ6JqVUUndyEYrkfNv+dCA7I47/\n" +
+            "l5hH1hYgGnQ3d7AVtyvvL9NFT3AnAgMBAAECgYEAvo9ewdJC/LFxpstLdLKpdxey\n" +
+            "BEQqvkq3sMnHKZY/H3xBso2fNFOJQIiXIbLUIMsuhFWCEvv+5cmpOZxBmAOtsK5c\n" +
+            "cLF7VP3D5Ix1q4Cg9MYl1eabYkkHKd3lfsWQwMPZgEbLCOpMZwtMUIGTgpNtVy5f\n" +
+            "iusbXikkycrGib5rUbkCQQDv2dEQOIZ2SGe0iY+FB1drhXNliAqAjhuVkz+GC+E8\n" +
+            "hQEPbeIsxgusElfKzDPZdrvoMzVmzq4qDiBdMrNniwFzAkEA07ZF8lhrWx3cFK2H\n" +
+            "lbNP/IokXZp27bJBnddLEk5Z+yK3R+nyqVoesUwqWqDvNmE6evLnztdtlAiyxTkk\n" +
+            "BqaZfQJAY4gEFbMfOV56ipS7Ff0h7eCHLo7xrL9L+xoLtlifszmOYxS6UtIsbc9+\n" +
+            "1w0+RvParlTxyCPkaldKXuJ3SHSKuQJAX2DNkEmfS/Re0v3+iEW5Mke17GmLTLiy\n" +
+            "8P4uNvgPd6GDOqW+CeeszilHJ387ZZ9V7lACeN/64OcuSsXcYhaqLQJAVxEBN0d3\n" +
+            "pI5fixHw8Bahw4N2lIq+poXHNRHjkdO+6Qwe6Y9TBn0rtAaqZD3adX5pQcKUUP/C\n" +
+            "xg/EwCxcMeC84A==\n" +
+            "-----END PRIVATE KEY-----\n";
+    public static final String PUBLIC_KEY_STRING
+            = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDGW02zxDd47c5Wty7P2thw2Df2\n" +
+            "4n9Abo4B5pZVg48BKAKhQ2a4KSNe2N8WOozBbOWpmO+74NytfaGwpUnFTNWOHmLD\n" +
+            "IcsR87hd7o7tl3fPdQSHh0nRw7VkOialVFJ3chGK5Hzb/nQgOyOO/5eYR9YWIBp0\n" +
+            "N3ewFbcr7y/TRU9wJwIDAQAB\n" +
+            "-----END PUBLIC KEY-----\n";
 
     public static final String KEY_SHA = "SHA";
     public static final String KEY_MD5 = "MD5";
     public static final String KEY_ALGORITHM = "RSA";
     public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
+
     public static void main(String[] args) throws Exception {
         PrivateKey privateKey = getPrivateKeyFromPem();
-        PublicKey publicKey = getPublicKeyFromPem();
+//        PublicKey publicKey = getPublicKeyFromPem();
+//        PrivateKey privateKey = getPrivateKeyFromString();
+        PublicKey publicKey = getPublicKeyFromString();
         String data = "hello world";
 
         /*************************************************************************************************/
@@ -66,6 +95,7 @@ public class RSATest {
 
     /**
      * 用私钥对信息生成数字签名
+     *
      * @param data       加密数据
      * @param privateKey 私钥
      * @return
@@ -80,6 +110,7 @@ public class RSATest {
 
     /**
      * 校验数字签名
+     *
      * @param data      加密数据
      * @param publicKey 公钥
      * @param sign      数字签名
@@ -95,6 +126,7 @@ public class RSATest {
 
     /**
      * 私钥解密
+     *
      * @param data       密文
      * @param privateKey 私钥
      * @return
@@ -109,6 +141,7 @@ public class RSATest {
 
     /**
      * 用公钥解密
+     *
      * @param data      密文
      * @param publicKey 公钥
      * @return
@@ -123,6 +156,7 @@ public class RSATest {
 
     /**
      * 用公钥加密
+     *
      * @param data      明文
      * @param publicKey 公钥
      * @return
@@ -137,6 +171,7 @@ public class RSATest {
 
     /**
      * 用私钥加密
+     *
      * @param data       明文
      * @param privateKey 私钥
      * @return
@@ -149,7 +184,7 @@ public class RSATest {
         return cipher.doFinal(data);
     }
 
-    // 获取私匙
+    // 获取私匙 from pem file
     public static PrivateKey getPrivateKeyFromPem() throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(PRIVATE_KEY_PEM));
         String s = br.readLine();
@@ -162,16 +197,53 @@ public class RSATest {
         BASE64Decoder base64decoder = new BASE64Decoder();
         byte[] b = base64decoder.decodeBuffer(str);
 
-        // 生成私匙
+        //生成私钥
         KeyFactory kf = KeyFactory.getInstance("RSA");
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(b);
         PrivateKey privateKey = kf.generatePrivate(keySpec);
         return privateKey;
     }
 
-    // 获取公钥
+    //获取私钥 from string
+    public static PrivateKey getPrivateKeyFromString() throws Exception {
+        BufferedReader br = new BufferedReader(new StringReader(PRIVATE_KEY_PCKS8));
+        String s = br.readLine();
+        String str = "";
+        s = br.readLine();
+        while (s.charAt(0) != '-') {
+            str += s + "\r";
+            s = br.readLine();
+        }
+        BASE64Decoder base64Decoder = new BASE64Decoder();
+        byte[] b = base64Decoder.decodeBuffer(PRIVATE_KEY_PCKS8);
+
+        //生成私钥
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(b);
+        PrivateKey privateKey = kf.generatePrivate(keySpec);
+        return privateKey;
+    }
+
+    // 获取公钥 from pem file
     public static PublicKey getPublicKeyFromPem() throws Exception {
         BufferedReader br = new BufferedReader(new FileReader(PUBLIC_KEY_PEM));
+        String s = br.readLine();
+        String str = "";
+        s = br.readLine();
+        while (s.charAt(0) != '-') {
+            str += s + "\r";
+            s = br.readLine();
+        }
+        BASE64Decoder base64decoder = new BASE64Decoder();
+        byte[] b = base64decoder.decodeBuffer(str);
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(b);
+        PublicKey pubKey = kf.generatePublic(keySpec);
+        return pubKey;
+    }
+    //获取公钥 from string
+    public static PublicKey getPublicKeyFromString() throws Exception {
+        BufferedReader br = new BufferedReader(new StringReader(PUBLIC_KEY_STRING));
         String s = br.readLine();
         String str = "";
         s = br.readLine();
